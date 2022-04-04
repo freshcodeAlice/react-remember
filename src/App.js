5fe05f141bd459de49f36b777104fc838cf9881f
freshcodeAlice <freshjs.ofte@gmail.com>
2022-04-04 19:13:26 +0300

    Task: Level up to Parent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from "react";
 import './App.css';
 import Switcher from './components/Switcher'
 
-function App(props) {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgc: 'smile'
+    }
+  }
+
+changeBgc = () => {
+this.setState({
+  bgc: this.state.bgc === 'smile' ? 'sad' : 'smile'
+})
+}
+
+render() {
+  const {bgc} = this.state
   return (
-    <div className="App">
-      <Switcher />
+    <div className={bgc}>
+      <Switcher bgColor={bgc} changeParentBackground={this.changeBgc}/>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/Switcher.js
+++ src/components/Switcher.js
@@ -17,13 +17,15 @@ class Switcher extends React.Component {
         this.setState({
             mood: !this.state.mood,
             moodText: this.state.moodText === 'smile' ? 'sad' : 'smile'
-        })
+        });
+        this.props.changeParentBackground();
     }
 
     render() {
         const {moodText, mood} = this.state;
+        const {bgColor} = this.props;
         const moodPic = mood ? smile : sad;
-        const cn = `switcher-container ${moodText}`;
+        const cn = `switcher-container ${bgColor}`;
   
         return (
             <div className={cn}>
@@ -36,3 +38,10 @@ class Switcher extends React.Component {
 }
 
 export default Switcher
+
+
+
+// Три типа взаимодействия компонент
+// Parent -> Child - props
+// Child -> Parent - callback
+// Child -> Child (Sibling) - Parent
