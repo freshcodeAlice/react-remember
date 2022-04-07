6382eaf5ce0a744a64535d416a2ec49c5f8c39ae
freshcodeAlice <freshjs.ofte@gmail.com>
2022-04-07 19:39:27 +0300

    Resolve task

--- src/components/CalculateForm/index.js
+++ src/components/CalculateForm/index.js
@@ -5,33 +5,60 @@ class CalculateForm extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-           dollar: 0,
-           hrivna: 0,
+           dollar: "",
+           hrivna: "",
+           valuta: null
         }
+        this.koefficient = 29.40;
     }
 
 
     handleInput = (event) => {
         const {name, value} = event.target;
                 this.setState({
-                    [name]: value
+                    [name]: value,
+                    valuta: name
                 });
         }
 
     handleSubmit = (event) => {
         event.preventDefault();
+        this.calculateMoney();
     }
 
+    calculateMoney = () => {
+        const {dollar, hrivna, valuta} = this.state;
+        if(valuta === 'hrivna') {
+            const summaInDollars = Math.floor(hrivna / this.koefficient);
+            this.setState({
+                dollar: summaInDollars
+            });
+        } else if (valuta === 'dollar') {
+            const summaInHrivnas = Math.floor(dollar * this.koefficient);
+            this.setState({
+                hrivna: summaInHrivnas
+            });
+        } 
+    }
+
+
     render() {
-        const {isEmailValid} = this.state;
         return(
             <form className="form-container" onSubmit={this.handleSubmit}>
-                <input type="text" value={this.state.email} name="dollar" onChange={this.handleInput} />
-                <input type="text" value={this.state.password} name="hrivna"  onChange={this.handleInput}/>
-                <button type="submit">Click me!</button>
+                <input type="text" value={this.state.dollar} name="dollar" onChange={this.handleInput} />
+                <input type="text" value={this.state.hrivna} name="hrivna"  onChange={this.handleInput}/>
+                <button type="submit">Convert</button>
+                <button type="reset">Clear</button>
             </form>
         )
     }
 }
 
 export default CalculateForm;
+
+
+
+
+// Parent -> Child -- props
+// Child -> Parent -- callback
+// Child -> Child -- Parent
