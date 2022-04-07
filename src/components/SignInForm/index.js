0f612d656ad0f60813b620bd26e8975dfe5ff0ba
freshcodeAlice <freshjs.ofte@gmail.com>
2022-04-07 18:14:08 +0300

    Banal Validation

--- src/components/SignInForm/index.js
+++ src/components/SignInForm/index.js
@@ -7,17 +7,28 @@ class SignInForm extends React.Component {
         this.state = {
             email: "",
             password: "",
-    
+            isEmailValid: false
         }
     }
 
 
     handleInput = (event) => {
-        console.log(event.target)
         const {name, value} = event.target;
-        this.setState({
-             [name]: value
-        })
+       
+            const verdict = this.validateEmail(event.target);
+
+                this.setState({
+                    isEmailValid: verdict,
+                    [name]: value
+                });
+        }
+
+    validateEmail = (target) => {
+        const {name, value} = target;
+        if(name === "email" && value.length > 6) {
+            return true
+        } 
+        return false
     }
 
 
@@ -42,9 +53,10 @@ console.dir(event.target);
     }
 
     render() {
+        const {isEmailValid} = this.state;
         return(
             <form className="form-container" onSubmit={this.handleSubmit}>
-                <input type="text" value={this.state.email} onChange={this.handleInput} name="email"/>
+                <input type="text" value={this.state.email} onChange={this.handleInput} name="email" className={isEmailValid ? "validEmail" : "invalidEmail"}/>
                 <input type="password" value={this.state.password} name="password"  onChange={this.handleInput}/>
                 <button type="submit">Click me!</button>
             </form>
